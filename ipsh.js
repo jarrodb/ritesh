23d478c18c4d6a3e4399356fff10c561a0291927
Jarrod Baumann <jarrod@theos.org>
2012-10-29 14:22:04 -0500

    Updated control handler as well as tab completion (basic)

--- ipsh.js
+++ ipsh.js
@@ -35,8 +35,8 @@ var Host = function(terminal, kwargs) {
 
 }; Host.prototype = {
     //public:
-    inputHandler: function(argv) {
-        this.kernel.input(argv);
+    inputHandler: function(input, ctrl) {
+        this.kernel.input(input, ctrl);
     },
 
     write: function(output) {
@@ -56,22 +56,118 @@ var Kernel = function(terminal, kwargs) {
     this.terminal = terminal;
     this.fs = new Filesystem();
     this.user = new User();
-    this.name = (kwargs.name) ? kwargs.name : 'jarrod';
+    this.name = 'jarrod';
 
 
 }; Kernel.prototype = {
     //public:
-    input: function(argv) {
-        this._command(argv);
+    input: function(input, ctrl) {
+        var show_prompt = true;
+        var append = false;
 
-        if (!this.user.sudoInput)
-            this.prompt();
+        switch(this.terminal.inputChar) {
+            case termKey.ETX:
+                //this.terminal.close(); // ^C
+                break;
+            case termKey.NAK:
+                show_prompt = false;
+                this.terminal._clearLine();
+                break;
+            case termKey.EOT:
+                this._command('exit');
+                break;
+            case termKey.TAB:
+                var buffer = this.terminal._getLine();
+                var name = buffer.split(' ').slice(-1)[0]; // last element
+
+                cmds = this.fs.command(name, true); // search=true
+                if (cmds.length == 1) {
+                    show_prompt = false;
+                    var rest = cmds[0].substring(name.length)+' ';
+                    this.terminal.type(rest);
+                } else {
+                    this.write('%n');
+                    this.write(cmds.join('%n'));
+                    append = buffer;
+                    show_prompt = true;
+                }
+                break;
+            default:
+                this._command(input);
+                break;
+        }
+
+        if (!this.user.sudoInput) {
+            if (show_prompt)
+                this.prompt();
+            if (append) {
+                this.terminal.type(append);
+                append = false;
+            }
+        }
     },
 
     write: function(output) { this.terminal.write(output); },
     prompt: function() {
         this.terminal.ps = this._prompt();
         this.terminal.prompt();
+    },
+
+    mkdir: function(name, path) {
+        var inode = this.fs._name_in_path(name, path);
+        if (!inode) {
+            var new_inode = new Inode({
+                'name': name,
+                'type': 'dir',
+                'path': path,
+                'access': function(kernel, argv) {
+                    return { 'output': '' } // normal operation
+                }
+            });
+            var i = this.fs.add_inode(new_inode);
+        } else if (inode.type != "dir") {
+            throw {'output': 'File exists.'}
+        } else if (inode.type == "dir") {
+            throw {'output': 'Directory exists.'}
+        }
+        return true;
+    },
+
+    touch: function(name, path) {
+        var inode = this.fs._name_in_path(name, path);
+        if (!inode) {
+            var new_inode = new Inode({
+                'name': name,
+                'type': 'file',
+                'path': path,
+                'access': function(kernel, argv) {
+                    return { 'output': '' }
+                }
+            });
+            var i = this.fs.add_inode(new_inode);
+        } else {
+            throw {'output': 'File exists.'};
+        }
+    },
+
+    cd: function(name, path) {
+        var inode = this.fs._name_in_path(name, path);
+        if (inode) { // path exists
+            if (inode.type != 'dir')
+                throw {'output': 'Not a directory.'};
+
+            this.user.pwd = path+name+'/';
+
+            res = inode.access(this);
+            switch(res.action) {
+                case 'redirect':
+                    window.open(res.value, '', '');
+                    break;
+                default:
+                    break;
+            }
+        } else
+            throw {'output': 'No such directory.'};
     },
 
     //private:
@@ -165,24 +261,42 @@ var Filesystem = function(kwargs) {
 
 }; Filesystem.prototype = {
 
-    command: function(cmd) {
+    command: function(cmd, search) {
+        search = (search) ? true : false;
+        var path = this.PATH;
+        var results = [];
+
+        //if (path.indexOf(pwd) == -1)
+        //    path.push(this.PATH.push(pwd)); // "."
+
+        if (search && !cmd) return [];
+
         for (var j=0; j < this.PATH.length; j++) {
             var path = this.PATH[j];
             var inodes = this._inodes_for_path(path);
             for (var k=0; k < inodes.length; k++) {
                 var inode = inodes[k];
                 var obj = this.inodes[inode];
-                if (obj.name == cmd && obj.type == "cmd") {
+
+                if (search) {
+                    if (obj.name.lastIndexOf(cmd, 0) == 0) {
+                        results.push(obj.name);
+                    }
+                }
+                else if (obj.name == cmd && obj.type == "cmd") {
                     return obj
                 }
             }
         }
+        return (search) ? results : undefined;
     },
 
     add_inode: function(inode) {
         var dir = this.dirs[inode.path];
-        if (!dir)
+        if (dir == undefined) {
             this.dirs[inode.path] = {'inodes': []};
+        }
+        var dir = this.dirs[inode.path];
 
         guid = this._guid();
         this.inodes[guid] = inode;
@@ -202,20 +316,47 @@ var Filesystem = function(kwargs) {
         return results;
     },
 
+    iterate_paths: function(pwd, dirs) {
+        paths = [];
+        if (dirs.substring(0,1) == '/')
+            // string leading / for split purposes
+            dirs = dirs.substring(1);
+        dirs = dirs.split('/');
+
+        for (var k=0; k < dirs.length; k++) {
+            var name = dirs[k];
+
+            var fullpath = pwd+dirs.slice(0,k).join('/');
+            if (k != 0)
+                fullpath += '/'; // add / when its not the root
+
+            if (name)
+                paths.push({'name': name, 'fullpath': fullpath});
+        }
+        return paths;
+    },
+
+    path_exists: function(path) {
+        return (this.dirs[path] != undefined);
+    },
+
     // private:
-    _name_exists_in_path: function(name, path) {
+
+    _name_in_path: function(name, path) {
+        // return the inode object for name in path
         var func = function(inode) {
             if (inode.name == name)
-                return true;
+                return inode;
             return false;
-        }
+        };
 
         // results : Array containing results from name comparison
         //           ... not optimal, I know.
         var results = this.map_inodes_in_path(func, path);
 
         for (var k=0; k < results.length; k++) {
-            if (results[k]) return true;
+            // return the inode id
+            if (results[k]) return results[k];
         }
         return false;
     },
@@ -223,6 +364,12 @@ var Filesystem = function(kwargs) {
     _inodes_for_path: function(path) {
         var d = this.dirs[path];
         return (d) ? d.inodes : [];
+    },
+
+    _sanitize_path: function(path) {
+        if (path.substring(0, 1) == "/")
+            return path;
+        return '/'+path;
     },
 
     _guid: function() {
@@ -289,7 +436,7 @@ var id = new Inode({
         if (kernel.user.is_root())
             output += '(root)';
         else
-            output += '(guest)';
+            output += '(gest)';
         return {'output': output}
     }
 });
@@ -373,38 +520,26 @@ var mkdir = new Inode({
         var usage = argv[0]+' <dir>';
         var pwd = kernel.user.pwd;
 
-        // check for '/' in the name (split later, reject now)
-        // abstract this inode creation (we do it below also)
-
         // only one file at a time until i add a loop
         if (argv.length != 2 )
             return {'output': usage}
 
-        var dirs = argv[1].split('/');
-        for (var k=0; k < dirs.length; k++) {
-            var name = dirs[k];
+        try {
+            paths = kernel.fs.iterate_paths(pwd, argv[1]);
+            for (var i in paths) {
+                obj = paths[i];
 
-            var fullpath = pwd+dirs.slice(0,k).join('/');
-            if (k != 0)
-                fullpath += '/'; // add / when its not the root
-
-            if (kernel.fs._name_exists_in_path(name, fullpath))
-                return {'output': 'File exists.'}
-
-            var new_inode = new Inode({
-                'name': name,
-                'type': 'dir',
-                'path': fullpath,
-                'access': function(kernel, argv) {
-                    return { 'output': '' } // normal operation
+                var objpath = obj.fullpath+obj.name+'/';
+                if (!kernel.fs.path_exists(objpath)) {
+                    // buggy
+                    kernel.mkdir(obj.name, obj.fullpath);
                 }
-            });
-            var i = kernel.fs.add_inode(new_inode);
-            console.log('added: '+i);
+            }
+        } catch(err) {
+            console.log(err);
+            return err;
         }
-        console.log(kernel.fs.inodes);
-
-        return {'output': ''}
+        return {'output': ''};
     }
 });
 
@@ -414,25 +549,23 @@ var touch = new Inode({
     'path': '/usr/bin',
     'access': function(kernel, argv) {
         var usage = argv[0]+' <file>';
+        var pwd = kernel.user.pwd;
 
         // only one file at a time until i add a loop
         if (argv.length != 2)
             return {'output': usage}
 
         // add sanity
-        if (kernel.fs._name_exists_in_path(argv[1], kernel.user.pwd))
-            return {'output': 'File exists.'}
-
-        var new_inode = new Inode({
-            'name': argv[1],
-            'type': 'file',
-            'path': kernel.user.pwd,
-            'access': function(kernel, argv) {
-                return {'output': []}
-            }
-        });
-        kernel.fs.add_inode(new_inode);
-
+        try {
+            // touch just looks to create in the last path
+            var obj = kernel.fs.iterate_paths(pwd, argv[1]).slice(-1)[0];
+            if (kernel.fs.path_exists(obj.fullpath)) {
+                kernel.touch(obj.name, obj.fullpath);
+            } else
+                return {'output': 'No such file or directory.'};
+        } catch(err) {
+            return err;
+        }
         return {'output': ''}
     }
 });
@@ -465,6 +598,10 @@ var cd = new Inode({
     'path': '/usr/bin',
     'access': function(kernel, argv) {
         var dir = argv[1];
+        var pwd = kernel.user.pwd;
+
+        if (argv.length != 2)
+            return {'output': 'cd <directory>'}
 
         if (dir == "..") {
             if (!(kernel.user.pwd == '/')) {
@@ -480,28 +617,17 @@ var cd = new Inode({
             return;
         }
 
-        var inodes = kernel.fs._inodes_for_path(kernel.user.pwd);
-        for (var k=0; k < inodes.length; k++) {
-            var obj = kernel.fs.inodes[inodes[k]];
-            if (obj.name == argv[1]) {
-                if (obj.type == "dir") {
-                    kernel.user.pwd += obj.name+'/';
+        try {
+            if (argv[1].substring(0,1) == '/')
+                pwd = '/';
 
-                    res = obj.access(kernel);
-                    switch(res.action) {
-                        case 'redirect':
-                            window.open(res.value, '', '');
-                            break;
-                        default:
-                            break;
-                    }
-                    return res;
-                } else {
-                    return {'output': argv[1]+": Not a directory."}
-                }
-            }
+            var obj = kernel.fs.iterate_paths(pwd, argv[1]).slice(-1)[0];
+            console.log(obj);
+            kernel.cd(obj.name, obj.fullpath);
+        } catch(err) {
+            return err;
         }
-        return {'output': argv[1]+": No such file or directory."}
+        return {'output': ''}
     }
 });
 
@@ -539,7 +665,7 @@ var aboutme = new Inode({
 var root_README = new Inode({
     'name':'README',
     'type':'file',
-    'path':'/root',
+    'path':'/root/',
     'access': function(kernel, argv) {
         return {
             'output': [
